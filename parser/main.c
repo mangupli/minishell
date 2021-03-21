64f5ab73521b1c0499606638c53789cf206c00a5
mstoneho <mstoneho@student.21-school.ru>
2021-03-21 22:45:58 +0300

    sadfghj

--- parser/main.c
+++ parser/main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../includes/parseader.h"
+#include "../includes/minishell.h"
 
 
 int main(int argc, char *argv[], char *env[])
---
+++ parser/path_parser.c
@@ -0,0 +1,11 @@
+
+#include "../includes/parseader.h"
+#include "../includes/minishell.h"
+
+t_path	*path_parser(t_env *envs)
+{
+	int i;
+
+	i = 0;
+	while (envs[i].name != '')
+}
